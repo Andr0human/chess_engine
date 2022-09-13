d62435c2423ce8dd3575a349e93b50b550318f53
Andr0human <ayushsinha132001@gmail.com>
2022-09-13 23:58:20 +0530

    Code refactoring.

--- move_utils.cpp
+++ move_utils.cpp
@@ -3,8 +3,9 @@
 #include "move_utils.h"
 
 
-void decode_move(const int move) {
-
+void
+decode_move(const int move)
+{
     const int ip  = move & 63;
     const int fp  = (move >> 6) & 63;
     const int pt  = (move >> 12) & 7;
@@ -20,7 +21,8 @@ void decode_move(const int move) {
     cout << "Piece_color : " << (cl == 1 ? "White\n" : "Black\n");
     cout << "Move_Strength : " << pr << '\n';
 
-    if (pt == 1 && ((1ULL << fp) & Rank18)) {
+    if (pt == 1 && ((1ULL << fp) & Rank18))
+    {
         cout << "Promoted_to : ";
         if (ppt == 0) cout << "Bishop\n";
         else if (ppt == 1) cout << "Knight\n";
@@ -33,28 +35,26 @@ void decode_move(const int move) {
 
 
 
-
-
-inline int gen_base_move(const chessBoard &_cb, int ip) {
-
+inline int
+gen_base_move(const chessBoard &_cb, int ip)
+{
     const int pt =  _cb.board[ip] & 7;
     const int side = _cb.color << 20;
 
     const int base_move = side + (pt << 12) + ip;
     return base_move;
-
 }
 
-inline void add_cap_moves(const int ip, uint64_t endSquares, const int base_move,
-                          const chessBoard &_cb, MoveList &myMoves) {
+inline void
+add_cap_moves(const int ip, uint64_t endSquares,
+    const int base_move, const chessBoard &_cb, MoveList &myMoves)
+{
+    const auto cap_priority = [] (const int pt, const int cpt)
+    { return (cpt - pt + 12); };
 
-    const auto cap_priority = [] (const int pt, const int cpt) {
-        return (cpt - pt + 12);
-    };
-
-    const auto encode_full_move = [] (const int base, const int fp,
-                                 const int cpt, const int move_pr) {
-
+    const auto encode_full_move = [] (const int base,
+            const int fp, const int cpt, const int move_pr)
+    {
         const int e_move = base + (move_pr << 21) + (cpt << 15) + (fp << 6);
         return e_move;
     };
@@ -62,7 +62,8 @@ inline void add_cap_moves(const int ip, uint64_t endSquares, const int base_move
     const int pt = _cb.board[ip] & 7;
     // const int base_str = 8;
 
-    while (endSquares != 0) {
+    while (endSquares != 0)
+    {
         const int fp = next_idx(endSquares);
         const int cpt = _cb.board[fp] & 7;
         const int move_priority = cap_priority(pt, cpt);
@@ -70,68 +71,63 @@ inline void add_cap_moves(const int ip, uint64_t endSquares, const int base_move
         const int enc_move = encode_full_move(base_move, fp, cpt, move_priority);
         myMoves.Add(enc_move);
     }
-
 }
 
-inline void add_quiet_moves(uint64_t endSquares, const int base_move,
-                            const chessBoard &_cb, MoveList &myMoves) {
-
-    const auto encode_full_move = [] (const int base, const int fp, const int pr) {
-
+inline void
+add_quiet_moves(uint64_t endSquares,
+    const int base_move, const chessBoard &_cb, MoveList &myMoves)
+{
+    const auto encode_full_move = [] (const int base, const int fp, const int pr)
+    {
         const int e_move = base + (pr << 21) + (fp << 6);
         return e_move;
     };
 
-    while (endSquares  != 0) {
+    while (endSquares  != 0)
+    {
         const int fp = next_idx(endSquares);
         const int move_priority = 0;
         const int enc_move = encode_full_move(base_move, fp, move_priority);
         myMoves.Add(enc_move);
     }
-
 }
 
-void add_move_to_list(const int ip, uint64_t endSquares,
-                      const chessBoard &_cb, MoveList &myMoves) {
-
+void
+add_move_to_list(const int ip, uint64_t endSquares,
+    const chessBoard &_cb, MoveList &myMoves)
+{
     const uint64_t cap_Squares = endSquares & ALL(EMY);
     const uint64_t quiet_Squares = endSquares ^ cap_Squares;
     const int base_move = gen_base_move(_cb, ip);
 
     add_cap_moves(ip, cap_Squares, base_move, _cb, myMoves);
     add_quiet_moves(quiet_Squares, base_move, _cb, myMoves);
-
 }
 
 
 
-
-
-void add_quiet_pawn_moves(uint64_t endSquares, const int shift,
-                          const chessBoard &_cb, MoveList & myMoves) {
-
+void
+add_quiet_pawn_moves(uint64_t endSquares,
+    const int shift, const chessBoard &_cb, MoveList & myMoves)
+{
     const int pt = 1;
     const int side = _cb.color << 20;
 
     const int base_move = side + (pt << 12);
 
-    while (endSquares != 0) {
+    while (endSquares != 0)
+    {
         const int fp = next_idx(endSquares);
         const int ip = fp + shift;
         const int enc_move = base_move + (fp << 6) + ip;
         myMoves.Add(enc_move);
     }
-
 }
 
 
-
-
-
-
-
-uint64_t pawn_atk_sq(const chessBoard& _cb, const int enemy) {
-
+uint64_t
+pawn_atk_sq(const chessBoard& _cb, const int enemy)
+{
     const auto side = _cb.color ^ enemy;
     const auto inc  = 2 * side - 1;
     const auto pawns = _cb.Pieces[8 * side + 1];
@@ -141,43 +137,41 @@ uint64_t pawn_atk_sq(const chessBoard& _cb, const int enemy) {
            shifter(pawns & LeftAttkingPawns , 8 - inc);
 }
 
-uint64_t bishop_atk_sq(const int __pos, const uint64_t _Ap) {
-
-    const auto area = [__pos, _Ap] (const uint64_t *table, const auto &__func) {
-        return table[__func(table[__pos] & _Ap)];
-    };
+uint64_t
+bishop_atk_sq(const int __pos, const uint64_t _Ap)
+{
+    const auto area = [__pos, _Ap] (const uint64_t *table, const auto &__func)
+    { return table[__func(table[__pos] & _Ap)]; };
 
     return plt::diag_Board[__pos] ^
           (area(plt::ulBoard, lSb_idx) ^ area(plt::urBoard, lSb_idx)
          ^ area(plt::dlBoard, mSb_idx) ^ area(plt::drBoard, mSb_idx));
 }
 
-uint64_t knight_atk_sq(const int __pos, const uint64_t _Ap) {
-    return plt::NtBoard[__pos] + (_Ap - _Ap);
-}
+uint64_t
+knight_atk_sq(const int __pos, const uint64_t _Ap)
+{ return plt::NtBoard[__pos] + (_Ap - _Ap); }
 
-uint64_t rook_atk_sq(const int __pos, const uint64_t _Ap) {
-
-    const auto area = [__pos, _Ap] (const uint64_t *table, const auto &__func) {
-        return table[__func(table[__pos] & _Ap)];
-    };
+uint64_t
+rook_atk_sq(const int __pos, const uint64_t _Ap)
+{
+    const auto area = [__pos, _Ap] (const uint64_t *table, const auto &__func)
+    { return table[__func(table[__pos] & _Ap)]; };
 
     return plt::line_Board[__pos] ^
           (area(plt::uBoard, lSb_idx) ^ area(plt::dBoard, mSb_idx)
          ^ area(plt::rBoard, lSb_idx) ^ area(plt::lBoard, mSb_idx));
-
 }
 
-uint64_t queen_atk_sq(const int __pos, const uint64_t _Ap) {
-    return bishop_atk_sq(__pos, _Ap) ^ rook_atk_sq(__pos, _Ap);
-}
+uint64_t
+queen_atk_sq(const int __pos, const uint64_t _Ap)
+{ return bishop_atk_sq(__pos, _Ap) ^ rook_atk_sq(__pos, _Ap); }
 
 
 
-
-
-bool Incheck(const chessBoard &_cb) {
-
+bool
+Incheck(const chessBoard &_cb)
+{
     const int kpos = idx_no(KING(OWN));
     uint64_t res, Apieces = ALL_BOTH;
     const int emy = EMY;
@@ -201,8 +195,9 @@ bool Incheck(const chessBoard &_cb) {
     return false;
 }
 
-string print(int move, chessBoard &_cb) {
-    
+string
+print(int move, chessBoard &_cb)
+{    
     string res;
     if (!move) return "null";
 
@@ -213,35 +208,36 @@ string print(int move, chessBoard &_cb) {
     const int color = (_cb.board[ip] > 8 ? 1 : 0);
     const uint64_t Apieces = ALL_BOTH;
 
-    const auto idx_to_row = [](int value) {
-        return (char)('a' + value);
-    };
+    const auto idx_to_row = [](int value)
+    { return (char)('a' + value); };
     
-    const auto idx_to_col = [](int value) {
-        return (char)('1' + value);
-    };
+    const auto idx_to_col = [](int value)
+    { return (char)('1' + value); };
     
-    const auto add_to_string = [&res](char a, char b) {
+    const auto add_to_string = [&res](char a, char b)
+    {
         res.push_back(a);
         res.push_back(b);
     };
     
-    const auto fill_move = [&](uint64_t area, char piece) {
-
+    const auto fill_move = [&](uint64_t area, char piece)
+    {
         bool row = true, col = true, found = false;
 
         res = piece;
         area &= _cb.Pieces[OWN + _pt];
         area ^= 1ULL << ip;
 
-        while (area) {
+        while (area)
+        {
             found = true;
             const int idx = next_idx(area);
             const int x = idx & 7, y = (idx - x) >> 3;
             if (x == ip_x) col = false;
             if (y == ip_y) row = false;
         }
-        if (found) {
+        if (found)
+        {
             if (col) res += idx_to_row(ip_x);
             else if (row) res += idx_to_col(ip_y);
             else add_to_string(idx_to_row(ip_x), idx_to_col(ip_y));
@@ -255,7 +251,8 @@ string print(int move, chessBoard &_cb) {
     bool checks = Incheck(_cb);
     _cb.UnmakeMove();
     
-    if (_pt == 1) {
+    if (_pt == 1)
+    {
         bool enp = false;
         if (std::abs(fp_x - ip_x) == 1 && !_cpt) enp = true;
         
@@ -272,24 +269,32 @@ string print(int move, chessBoard &_cb) {
         else if (ppt == 2) res += "=R";
         else if (ppt == 3) res += "=Q";
         
-    } else if (_pt == 2) {
+    }
+    else if (_pt == 2)
+    {
         fill_move(bishop_atk_sq(fp, Apieces), 'B');
-
-    } else if (_pt == 3) {
+    }
+    else if (_pt == 3)
+    {
         fill_move(knight_atk_sq(fp, Apieces), 'N');
-
-    } else if (_pt == 4) {
+    }
+    else if (_pt == 4)
+    {
         fill_move(rook_atk_sq(fp, Apieces), 'R');
-
-    } else if (_pt == 5) {
+    }
+    else if (_pt == 5)
+    {
         fill_move(queen_atk_sq(fp, Apieces), 'Q');
-
-    } else if (_pt == 6) {
-        if (abs(fp_x - ip_x) == 2) {
+    }
+    else if (_pt == 6)
+    {
+        if (abs(fp_x - ip_x) == 2)
+        {
             if (fp == 2 || fp == 58) res = "O-O-O";
             else if (fp == 6 || fp == 62) res = "O-O";
         }
-        else {
+        else
+        {
             res += "K";
             if (_cpt) res += 'x';
             res += idx_to_row(fp_x);
@@ -301,15 +306,15 @@ string print(int move, chessBoard &_cb) {
     return res;
 }
 
-void print(const MoveList &myMoves, chessBoard &_cb) {
-
+void
+print(const MoveList &myMoves, chessBoard &_cb)
+{
     cout << "MoveCount : " << myMoves.size() << '\n';
     
     int move_no = 0;
-    for (const auto move : myMoves) {
+    for (const auto move : myMoves)
         cout << (++move_no) << "\t| " << print(move, _cb) << "\t| "
              << move << '\n';
-    }
 
     cout << endl;
 }
--- search_utils.h
+++ search_utils.h
@@ -8,8 +8,8 @@
 #include "movegen.h"
 #include <iostream>
 
-enum search_flag:int {
-
+enum search_flag:int
+{
     ofs = 7,
     maxMoves = 156, maxPly = 40,
     maxDepth = 36, default_search_time = 2,
@@ -22,17 +22,16 @@ enum search_flag:int {
     valUNKNOWN = 5567899,
     HASHEMPTY = 0, HASHEXACT = 1,
     HASHALPHA = 2, HASHBETA  = 3,
-    
 };
 
 
-class moveOrderClass {
-
+class moveOrderClass
+{
+    private:
     std::pair<int, double> moves[156];
     uint64_t mCount = 0;
 
     public:
-
     void initialise(MoveList& myMoves);
 
     void sortList(int best_move);
@@ -48,59 +47,71 @@ class moveOrderClass {
     uint64_t moveCount();
 
     void reset();
-
 };
 
-class search_data {
-
+class search_data
+{
+    private:
     int depth;
     std::pair<int, int> move[maxPly];
 
     public:
-
     int side2move;
     int max_ply, max_qs_ply;
     int pvAlpha[maxPly];
     
 
-    search_data() {
-        depth = max_ply = max_qs_ply = 0;
-    }
+    search_data()
+    { depth = max_ply = max_qs_ply = 0; }
 
-    int min3(int _X, int _Y, int _Z);
+    int
+    min3(int _X, int _Y, int _Z);
 
-    int max3(int _X, int _Y, int _Z);
+    int
+    max3(int _X, int _Y, int _Z);
 
-    int best_move();
+    int
+    best_move();
 
-    int last_depth();
+    int
+    last_depth();
 
-    double last_eval();
+    double
+    last_eval();
 
-    int eval();
+    int
+    eval();
 
-    void set_to_move(int pc);
+    void
+    set_to_move(int pc);
 
-    void update(int _d, int _e, int line[]);
+    void
+    update(int _d, int _e, int line[]);
 
-    void reset();
+    void
+    reset();
 
-    void set_depth_zero_move(int __m);
+    void
+    set_depth_zero_move(int __m);
 
-    bool is_part_of_pv(int __m);
+    bool
+    is_part_of_pv(int __m);
 
-    bool use_verification_Search();
+    bool
+    use_verification_Search();
 
-    bool move_verified();
+    bool
+    move_verified();
 
-    void init(int color, int zMove);
+    void
+    init(int color, int zMove);
 
-    void set_discard_result(int zMove);
-
+    void
+    set_discard_result(int zMove);
 };
 
-struct thread_search_info {
-
+struct thread_search_info
+{
     chessBoard Board;
     bool beta_cut, threads_available, time_left;
 
@@ -112,36 +123,43 @@ struct thread_search_info {
 
     thread_search_info ();
 
-    void set(chessBoard &tmp_board, MoveList &tmp, int t_dep, int ta, int tb, int tply, int pv_idx, int start);
+    void
+    set(chessBoard &tmp_board, MoveList &tmp, int t_dep, int ta, int tb, int tply, int pv_idx, int start);
 
-    uint64_t get_index();
+    uint64_t
+    get_index();
 
-    std::pair<int, int> result();
-
+    std::pair<int, int>
+    result();
 };
 
-struct play_board {
-
+struct play_board
+{
     chessBoard board;
     string commandline;
     static const int occ_pos_size = 300;
     uint64_t occured_positions[occ_pos_size];
     int pos_cnt, opponent_move;
 
-    play_board() {
+    play_board()
+    {
         board = chessBoard(
                 string("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
         pos_cnt = 0;
         opponent_move = 0;
     }
 
-    void init(const string fen) {
+    void
+    init(const string fen)
+    {
         board = fen;
         pos_cnt = 0;
         opponent_move = 0;
     }
 
-    void play_move(int move) {
+    void
+    play_move(int move)
+    {
         board.MakeMove(move);
         int pt = (move >> 12) & 7, cpt = (move >> 15) & 7;
         if (pt == 1 || cpt >= 1) pos_cnt = 0;                       // If pawn or a capture move, clear game history table
@@ -153,24 +171,21 @@ struct play_board {
         #endif
     }
 
-    void generate_history_table() {
-
+    void
+    generate_history_table()
+    {
         #if defined(TRANSPOSITION_TABLE_H)
         // for (int i = 0; i < pos_cnt; i++)
             // TT.RecordSearch(occured_positions[i]);
         #endif
-
     }
-
-
 };
 
-class ponder_list {
-
+class ponder_list
+{
     void index_swap(uint64_t i, uint64_t j);
 
-public:
-
+    public:
     int moves[maxMoves];
     int evals[maxMoves];
     int lines[maxMoves][maxDepth];
@@ -178,25 +193,25 @@ public:
 
     ponder_list() {mCount = 0;};
 
-    void setList(MoveList &myMoves);
+    void
+    setList(MoveList &myMoves);
 
-    void insert(int idx, int move, int eval, int line[]);
+    void
+    insert(int idx, int move, int eval, int line[]);
 
-    void show(chessBoard &board);
+    void
+    show(chessBoard &board);
 
-    void sortlist();
-
+    void
+    sortlist();
 };
 
-class test_position {
-
-public:
-
+class test_position
+{
+    public:
     string fen, name;
     int depth;
     uint64_t nodeCount = 0;
-
-    void generate_name(int index);
 
     test_position() {}
 
@@ -204,18 +219,21 @@ public:
 
     test_position(string f, int d, uint64_t nc, int index);
 
-    void print();
+    void
+    generate_name(int index);
 
+    void
+    print();
 };
 
 
-class movegen_test_position {
-
+class
+movegen_test_position
+{
     string pos_fen;
     vector<uint64_t> nodes;
 
-public:
-
+    public:
     movegen_test_position() {}
 
     movegen_test_position(const string& _fen, const vector<uint64_t>& _nodes)
@@ -224,29 +242,32 @@ public:
     movegen_test_position(const string&& _fen, const vector<uint64_t>&& _nodes)
     : pos_fen(std::move(_fen)), nodes(std::move(_nodes)) {}
 
-    uint64_t depth() const
+    uint64_t
+    depth() const
     { return nodes.size(); }
 
-    uint64_t expected_nodes(uint64_t depth) const
+    uint64_t
+    expected_nodes(uint64_t depth) const
     { return nodes[depth - 1]; }
 
-    void print() const {
+    // returns <max_depth, nodes_at_max_depth>
+    const std::pair<uint64_t, uint64_t>
+    max_node() const
+    { return std::make_pair(nodes.size(), nodes.back()); }
+
+    string
+    fen() const
+    { return pos_fen; }
+
+    void
+    print() const
+    {
         cout << "Fen = " << pos_fen << '\n';
         for (size_t i = 0; i < nodes.size(); i++)
             cout << "Depth " << (i + 1) << " = " << nodes[i] << '\n';
         cout << std::endl;
     }
-
-    // returns <max_depth, nodes_at_max_depth>
-    const std::pair<uint64_t, uint64_t> max_node() const
-    { return std::make_pair(nodes.size(), nodes.back()); }
-
-    string fen() const
-    { return pos_fen; }
-
 };
-
-
 
 
 
@@ -256,18 +277,25 @@ extern thread_search_info thread_data;
 extern play_board pb;
 extern ponder_list pdl;
 
-vector<string> split(const string &__s, char sep);
+vector<string>
+split(const string &__s, char sep);
+string
+strip(string __s);
+
 /**
  * @brief Returns a vector of strings stripped from both ends
  * 
  * @param list vector of strings
  * @return vector<string> 
  */
-string strip(string __s);
-vector<string> strip(vector<string> list);
-vector<uint64_t> to_nums(const vector<string>& list);
-vector<string> extract_argument_list(int argc, char *argv[]);
-void bits_on_board(uint64_t value);
+vector<string>
+strip(vector<string> list);
+vector<uint64_t>
+to_nums(const vector<string>& list);
+vector<string>
+extract_argument_list(int argc, char *argv[]);
+void
+bits_on_board(uint64_t value);
 
 
 #endif
