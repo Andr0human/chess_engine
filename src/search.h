3cf3427cd1123cae1b197ff0562c766bbb53a402
Andr0human <ayushsinha132001@gmail.com>
2025-02-02 18:57:55 +0530

    search improvements

--- src/search.h
+++ src/search.h
@@ -97,7 +97,7 @@ class SearchData
   Varray<pair<Move, Score>, MAX_DEPTH + 1> moveEvals;
 
   // Stores <move, time> for each move in each iteration.
-  Varray<pair<Move, uint64_t> , MAX_MOVES> moveTimes;
+  Varray<pair<Move, pair<Nodes, Nodes>> , MAX_MOVES> moveTimes;
 
   string
   ReadablePvLine(ChessBoard board) const noexcept
@@ -139,7 +139,7 @@ class SearchData
     moveEvals.add(make_pair(zeroMove, VALUE_ZERO));
 
     for (const Move move : movesArray)
-      moveTimes.add(make_pair(move, VALUE_ZERO));
+      moveTimes.add(make_pair(move, make_pair(0, 0)));
   }
 
   bool
@@ -205,13 +205,24 @@ class SearchData
   pair<Move, Score> LastIterationResult() const noexcept
   { return moveEvals.back(); }
 
-  uint64_t
+  Nodes
   TotalNodes() const noexcept
   {
     return std::accumulate(
-      moveTimes.begin(), moveTimes.end(), uint64_t(0),
-      [](uint64_t sum, const pair<Move, uint64_t>& moveTime) {
-        return sum + moveTime.second;
+      moveTimes.begin(), moveTimes.end(), Nodes(0),
+      [](Nodes sum, const pair<Move, pair<Nodes, Nodes>>& moveTime) {
+        return sum + moveTime.second.first;
+      }
+    );
+  }
+
+  Nodes
+  TotalQNodes() const noexcept
+  {
+    return std::accumulate(
+      moveTimes.begin(), moveTimes.end(), Nodes(0),
+      [](uint64_t sum, const pair<Move, pair<Nodes, Nodes>>& moveTime) {
+        return sum + moveTime.second.second;
       }
     );
   }
@@ -230,6 +241,7 @@ class SearchData
            << " | " << setw(5) << right << fixed << dep
            << " | " << setw(7) << right << fixed << setprecision(2) << evalConv
            << " | " << setw(8) << right << fixed  << TotalNodes()
+           << " | " << setw(8) << right << fixed  << TotalQNodes()
            << " | " << ReadablePvLine(pos) << endl;
   }
 
@@ -240,6 +252,7 @@ class SearchData
            << " | " << setw(5) << "Depth"
            << " | " << setw(7) << "Score"
            << " | " << setw(8) << "Nodes"
+           << " | " << setw(8) << "QNodes"
            << " | " << "PV" << "\n";
   }
 
@@ -256,7 +269,9 @@ class SearchData
     }
 
     std::sort(moveTimes.begin() + 1, moveTimes.end(), [](const auto &a, const auto &b) {
-      return a.second > b.second;
+      Nodes n1 = 2 * a.second.first + a.second.second;
+      Nodes n2 = 2 * b.second.first + b.second.second;
+      return n1 > n2;
     });
   }
 
@@ -264,13 +279,13 @@ class SearchData
   {
     int moveNo = 1;
     for (const auto& [move, tm] : moveTimes)
-      cout << moveNo++ << "\t| " << PrintMove(move, pos) << "\t| " << tm << endl;
+      cout << moveNo++ << "\t| " << PrintMove(move, pos) << "\t| " << tm.first << endl;
   }
 
   void
   InsertMoveToList(size_t moveNo)
   {
-    moveTimes[moveNo].second = (2 * nodes + qNodes) >> 1;
+    moveTimes[moveNo].second = make_pair(nodes, qNodes);
     ResetNodeCount();
   }
 
